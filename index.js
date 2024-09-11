ea6bbc1c15b654ef2c34a4ffc3b48e55222b731c
Hass <hassmroueh@gmail.com>
2024-09-11 18:40:14 -0400

    filter button almost complete

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 const carContainer = document.getElementById("car-container");
-const carText =document.getElementById("car-text");
+const carText = document.getElementById("car-text");
 const carForm = document.getElementById("car-form");
+const carArray = [];
 
 document.addEventListener("DOMContentLoaded", ()=>{
- carFetch()
-})
+ carFetch();            
+ carSubmit();   
+ filterBtn();                        
+})                                 
 
 function carFetch(){
     fetch("http://localhost:3000/Cars")
     .then(res=>res.json())
     .then(data=>{
-        Object.values(data).forEach(car=>{
-            carSubmit(car)
-        })
+        Object.values(data).forEach(car=>carArray.push(car))
     })
 }
 
-function carSubmit(car){
+function carSubmit(){
     carForm.addEventListener("submit", e=>{
         e.preventDefault()
-        if(carText.value === car.name||carText.value === car.name.toLowerCase()||carText.value === car.name.toUpperCase()){
-            carText.value = ""
-            makeCarCard(car)
+        const foundCar = carArray.find((car) => {
+            console.log(carText.value.trim().toLowerCase())
+            if(carText.value.trim() === car.name||carText.value.trim() === car.name.toLowerCase()||carText.value.trim() === car.name.toUpperCase()){
+                return car;
+            }
+        });
+        if(foundCar) {
+            carText.value = "";
+            makeCarCard(foundCar);
+            console.log(foundCar)
         }
     })
 }
 
 function makeCarCard(car){
     const carCard = document.createElement("div")
-    carCard.innerHTML = `<h1>${car.name}</h1><br><h3>${car.model}</h3><br><h4>${car.year}</h4><br><img src=${car.image} width = 400px height=200px><br><li>${car.price}</li><br><button id=car-button>show Information</button> <button id=delete-car>Delete</button>`
-   carCard.classList.add("carCard")
+    carCard.innerHTML = `<h1>${car.name}</h1><br><h3>${car.model}</h3><br><h4>${car.year}</h4><br><img src=${car.image} width = 400px height=200px><br><li>$${car.price}</li><br><button id=car-button>show Information</button> <button id=delete-car>Delete</button>`
+    carCard.classList.add("carCard")
     carContainer.append(carCard)
     const infoButton = carCard.querySelector("#car-button")
     infoButton.addEventListener("click",()=>{
@@ -37,15 +45,50 @@ function makeCarCard(car){
         infoButton.disabled = true;
     })
     const deleteBtn = carCard.querySelector("#delete-car");
-    deleteBtn.addEventListener("click", ()=>{
-        carCard.innerHTML = "";
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.addEventListener("click",()=>{
+     del(carCard)
     })
 }
 
 function showDeatilsOfCar(car,carCard){
-const p = document.createElement("p")
- p.classList.add("p")
- p.innerHTML = car.details
-carCard.appendChild(p)
+    const p = document.createElement("p");
+    p.classList.add("p");
+    p.innerHTML = car.details;
+    carCard.appendChild(p);
+}
+function filterBtn(){
+    const filterBtn = document.createElement("button");
+    filterBtn.textContent = "show cars Less then 90,000"
+    carContainer.appendChild(filterBtn)
+    filterBtn.addEventListener("click", ()=>{  //filterButton
+    filterBtn.disabled = true
+    const filteredDiv = document.createElement("div") ;  
+    filteredDiv.classList.add(".filterDiv");
+    carContainer.appendChild(filteredDiv)
+    const filteredCars = carArray.filter(car => parseFloat(car.price) <= 90000)
+    filteredCars.forEach(car=> {
+     let li = document.createElement("li")
+     li.textContent = `${car.name} ${car.model} $${car.price}`
+     filteredDiv.appendChild(li)
+    })
+    let closeBtn = document.createElement("button");
+    closeBtn.innerHTML = "close"
+    filteredDiv.appendChild(closeBtn)
+    closeBtn.addEventListener("click", ()=>{
+        del(filteredDiv)
+        filterBtn.disabled = false
+    })
+    })
+    //create a button that is at the top of the carContainer Div
+    //write an if statement that if the car is 90,000 or more create a div on the right of the page or bottom or clear the container div 
+    // inside that div have a little ul list that lists all the cars that are 90,000 or more
+    //also add a little delete button "make a delete function so we dont use repititive code."
+
 }
 
+
+
+function del(card){
+    card.innerHTML = "";
+}
